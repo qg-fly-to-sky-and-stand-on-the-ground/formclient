5752f09395e313413e3b1da14e03b9454e1a6f00
gayyou <zxc365912613@qq.com>
2019-11-18 22:24:58 +0800

    创建项目

---
+++ src/views/layout/Layout.vue
@@ -0,0 +1,26 @@
+<style lang="scss" scoped>
+.layout-container {
+  position: relative;
+  display: flex;
+  width: 100%;
+  height: 100vh;
+}
+</style>
+
+<template>
+  <div class="layout-container">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component';
+import LeftSection from "@/views/layout/leftSection/LeftSection.vue";
+import RightSection from "@/views/layout/rightSection/RightSection.vue";
+
+@Component
+export default class Layout extends Vue {
+
+}
+</script>
